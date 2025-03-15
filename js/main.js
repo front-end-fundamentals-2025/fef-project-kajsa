1720c90e3ed53d98fa817e57f6ad67454d6444f0
moakajsa <grka24fe@student.ju.se>
2025-03-15 17:21:12 +0100

    close when clicking outside tab

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   let cartButtons = document.querySelectorAll(".cart"); // Selects both desktop & mobile cart buttons
   let closeCart = document.querySelector(".close");
   let body = document.querySelector("body");
+  let cartTab = document.querySelector(".cart-tab");
 
-  if (cartButtons.length > 0 && closeCart) {
+  if (cartButtons.length > 0 && closeCart && cartTab) {
+    // Toggle cart on cart button click
     cartButtons.forEach((cart) => {
       cart.addEventListener("click", (event) => {
         event.preventDefault();
@@ -12,11 +14,21 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
 
+    // Close cart on close button click
     closeCart.addEventListener("click", () => {
       body.classList.remove("show-cart");
     });
+
+    // Close cart when clicking outside the cart-tab
+    document.addEventListener("click", (event) => {
+      if (!cartTab.contains(event.target) && !event.target.closest(".cart")) {
+        body.classList.remove("show-cart");
+      }
+    });
   } else {
-    console.error("Cart buttons or close button not found in the DOM");
+    console.error(
+      "Cart buttons, cart-tab, or close button not found in the DOM"
+    );
   }
 });
 
